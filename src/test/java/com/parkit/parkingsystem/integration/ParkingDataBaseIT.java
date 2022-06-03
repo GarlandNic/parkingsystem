528ce22c5b41320030cfce8aa302538674e7da46
GarlandNic <nicolas-garland@club-internet.fr>
2022-06-03 17:49:54 +0200

    complete of testParkingACar() with database (of tests) access functions, for counting and so on

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -13,7 +13,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -49,17 +56,101 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+    	int prevNbRowsTicketTable = nRowsTicketTable();
+    	int prevNbFalseAvailable = nAvailableParkingTable(false);
+    	int prevNbTrueAvailable = nAvailableParkingTable(true);
+    	
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actually saved in DB and Parking table is updated with availability
+
+        assertEquals(1 + prevNbRowsTicketTable, nRowsTicketTable());
+        assertEquals("ABCDEF", firstRegistrationNumberInDB());
+        assertEquals(1 + prevNbFalseAvailable, nAvailableParkingTable(false));
+        assertEquals(prevNbTrueAvailable - 1, nAvailableParkingTable(true));
     }
 
-    @Test
+	@Test
     public void testParkingLotExit(){
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
+        
+    private int nRowsTicketTable() {
+        int result = 0;
+    	String query = "select * from ticket";
+        Connection con = null;
+        try {
+            con = dataBaseTestConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                result++;
+            }
+            dataBaseTestConfig.closeResultSet(rs);
+            dataBaseTestConfig.closePreparedStatement(ps);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+        	dataBaseTestConfig.closeConnection(con);
+        }
+		return result;
+	}
+    
+	private String firstRegistrationNumberInDB() {
+        String result = "";
+    	String query = "select VEHICLE_REG_NUMBER from ticket";
+        Connection con = null;
+        try {
+            con = dataBaseTestConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                result = rs.getString("VEHICLE_REG_NUMBER");
+            }
+            dataBaseTestConfig.closeResultSet(rs);
+            dataBaseTestConfig.closePreparedStatement(ps);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+        	dataBaseTestConfig.closeConnection(con);
+        }
+		return result;
+	}
 
+	private int nAvailableParkingTable(boolean wantedAvailable) {
+		int result = 0;
+    	String query = "select AVAILABLE from parking";
+        Connection con = null;
+        try {
+            con = dataBaseTestConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+            	if(rs.getBoolean("AVAILABLE") == wantedAvailable) {
+            		result++;
+            	}
+            }
+            dataBaseTestConfig.closeResultSet(rs);
+            dataBaseTestConfig.closePreparedStatement(ps);
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}finally {
+        	dataBaseTestConfig.closeConnection(con);
+        }
+		return result;
+    }
 }
