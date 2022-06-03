bac50b49096830ab633e9c1c1597a937a7f2621b
GarlandNic <nicolas-garland@club-internet.fr>
2022-06-03 17:47:26 +0200

    Modifying the SQL request in case of multiple ticket for the same vehicle

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -7,9 +7,9 @@ public class DBConstants {
 
     public static final String SAVE_TICKET = "insert into ticket(PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME) values(?,?,?,?,?)";
     public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
-    public static final String GET_TICKET = "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME  limit 1";
+    public static final String GET_TICKET = "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? and t.OUT_TIME IS NULL order by t.IN_TIME ASC, t.OUT_TIME ASC limit 1";
     // get the oldest ticket for the vehicle matching the given registration number
-    // TODO: modify to get a ticket for a vehicle which is still inside the parking ; 2 possible solutions below:
+    // DONE: modify to get a ticket for a vehicle which is still inside the parking ; 2 possible solutions below:
     // and t.OUT_TIME IS NULL
     // order by t.IN_TIME ASC, t.OUT_TIME ASC
     // 			(ASC is default ; ASC => NULL is first)
